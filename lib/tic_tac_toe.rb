e49d69ae4834d218d76a1eb91f4be920a1d9e471
Steve Albonico <salbonico@gmail.com>
2019-01-12 18:28:58 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,13 +1,13 @@
 def display_board(board)
-  
+
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  
+
 puts "-----------"
-  
+
 puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  
+
 puts "-----------"
-  
+
 puts " #{board[6]} | #{board[7]} | #{board[8]} "
 
 end
@@ -15,18 +15,18 @@ end
 
 
 def valid_move?(board, index)
-  
+
 if index <0 || index >8
-    
+
 return false
-  
-elsif 
+
+elsif
 board[index] == " "
-    
+
 return true
-  
+
 else return false
-  
+
 end
 
 end
@@ -34,11 +34,11 @@ end
 
 
 def input_to_index(input)
-  
+
 index = input.to_i
-  
+
 index = index - 1
-  
+
 return index
 
 end
@@ -59,17 +59,17 @@ end
 
 
 def turn(board)
-  
+
 puts "Please enter 1-9:"
-  
+
 input = gets.strip
-  
+
 index=input_to_index(input)
-  
+
 if valid_move?(board,index) == true
-    
+
 move(board,index)
-  
+
 else turn(board)
 
 end

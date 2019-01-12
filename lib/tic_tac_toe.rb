ed2cf785444c711a343347dced8f67aebacfc2bd
Steve Albonico <salbonico@gmail.com>
2019-01-12 18:35:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,7 +77,22 @@ end
 end
 
 
+def turn_count(board)
+  count = 0
+  board.each do |position|
+    if position == "X"|| position == "O"
+      count += 1
+    end
+    return count
+  end
 
+  def current_player(board)
+    count = turn_count(board)
+    if count % 2 == 0
+      return "X"
+    else return "Y"
+    end
+  end
 
 
 
